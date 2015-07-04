cafefd63efd6770b32ef6274badb637ee4e981f3
Your Name <robjostone@gmail.com>
2015-07-04 14:39:18 +1000

    build profile when use created

--- app/models/business_profile.rb
+++ app/models/business_profile.rb
@@ -1,4 +1,7 @@
 class BusinessProfile < ActiveRecord::Base
 	 has_many :business_users #when have a model or a many to many you need to use plural
 	  has_many :users, through: :business_users
+
+	  extend FriendlyId
+	  friendly_id :name, use: :slugged
 end
